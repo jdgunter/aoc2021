e579286225d52904aa106926ad580f2a38f924d9
Joshua Gunter <joshua.gunter@rideco.com>
2021-12-06 09:29:20 -0500

    Improved solution + added timing

--- 6/6.py
+++ 6/6.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 
 class FishPopTable:
@@ -7,25 +8,25 @@ class FishPopTable:
         """Initialize a fish population table."""
         self.reproduction_time = reproduction_time
         self.days_until_mature = days_until_mature
-        self.pop_counts = [[None for _ in range(max_days + 1)] for _ in range(days_until_mature)]
-        # Fill in the base cases: after 0 days, every kind of fish has spawned 0 new fish.
-        for t in range(days_until_mature):
-            self.pop_counts[t][0] = 1
+        self.pop_counts = [None for _ in range(max_days + 1)]
+        self.pop_counts[0] = 1
     
     def get(self, init_internal_timer, day):
         """
         Get the population count for a fish starting with a value of 'init_interal_timer'
         for its internal time after 'day' days.
         """
-        if self.pop_counts[init_internal_timer][day]:
-            return self.pop_counts[init_internal_timer][day]
+        if day - init_internal_timer < 0:
+            return 1
+        elif self.pop_counts[day - init_internal_timer]:
+            return self.pop_counts[day - init_internal_timer]
         
         if init_internal_timer == 0:
             value = self.get(self.days_until_mature-1, day-1) + self.get(self.reproduction_time-1, day-1)
         else:
             value = self.get(init_internal_timer-1, day-1)
-        self.pop_counts[init_internal_timer][day] = value
-        return self.pop_counts[init_internal_timer][day]
+        self.pop_counts[day - init_internal_timer] = value
+        return self.pop_counts[day - init_internal_timer]
     
     def population_count(self, initial_internal_timers, num_days):
         """
@@ -40,12 +41,14 @@ class FishPopTable:
 
 def main():
     """Day 6 of Advent of Code."""
+    start_time = time.time()
     input_internal_timers = [int(n) for n in sys.stdin.read().split(",")]
     fish_pop_table = FishPopTable(9, 7, 256)
     # Part 1.
     print(fish_pop_table.population_count(input_internal_timers, 80))
     # Part 2.
     print(fish_pop_table.population_count(input_internal_timers, 256))
-        
+
+    print(f"\nElapsed time: {(time.time() - start_time) * 1000:.3f}ms")
 
 main()
